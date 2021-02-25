e8906a40beb4cc92a9c9c7959975e67c83b431dd
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-02-25 13:05:38 -0800

    Update ds_messenger.py

--- ds_messenger.py
+++ ds_messenger.py
@@ -1,1 +1,19 @@
-##class DirectMessenger:
+class DirectMessage:
+  def __init__(self):
+    recipient = None
+    message = None
+    timestamp = None
+
+
+class DirectMessenger:
+  def __init__(self, dsuserver=None, username=None, password=None):
+    token = None
+		
+  def send(self, message:str, recipient:str) -> bool:
+    pass
+		
+  def retrieve_new(self) -> DirectMessage:
+    pass
+ 
+  def retrieve_all(self) -> List:
+    pass
