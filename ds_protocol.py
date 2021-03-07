1ab357b88ded364a3d85b74334abfb89386c524d
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-03-07 13:29:27 -0800

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -4,8 +4,10 @@ from collections import namedtuple
 DataTuple = namedtuple('DataTuple', ['type','message','token'])
 MessageTuple = namedtuple('MessageTuple', ['type', 'message'])
 
-def send_directmessage():
-  pass
+def send_directmessage(user_token, msg, recipient):
+  
+  json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": str(time.time())}})
+  return json_directmessage
 
 def request_messages(user_token, msg):
   json_request = json.dumps({"token": user_token, "directmessage": msg})
