b2bdb141c0c6bd8f1900ab47f4b9f13ecc64be76
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-03-07 13:22:23 -0800

    Update ds_protocal.py

--- ds_protocal.py
+++ ds_protocal.py
@@ -1,1 +1,11 @@
 #For JSON SCRIPT
+import json, time
+from collections import namedtuple
+DataTuple = namedtuple('DataTuple', ['type','message','token'])
+MessageTuple = namedtuple('MessageTuple', ['type', 'message'])
+
+def send_directmessage():
+  pass
+
+def request_messages():
+  pass
