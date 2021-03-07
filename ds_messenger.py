4daf6884ddf7f54c4d4065319ea0fcbd52ed7afd
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-03-07 13:20:26 -0800

    Update ds_messenger.py

--- ds_messenger.py
+++ ds_messenger.py
@@ -2,41 +2,62 @@ import socket
 import ds_protocol
 
 class DirectMessage:
-  def __init__(self):
-    self.recipient = None
-    self.message = None
-    self.timestamp = None
+	def __init__(self):
+	self.recipient = None
+	self.message = None
+	self.timestamp = None
 
 
 class DirectMessenger:
-  def __init__(self, dsuserver=None, username=None, password=None):
-	self.token = None
-	self.dsuserver = None
-	self.username = None
-	self.password = None
+	def __init__(self, dsuserver=None, username=None, password=None):
+		self.token = None
+		self.dsuserver = None
+		self.username = None
+		self.password = None
+
+
+	def send(self, message:str, recipient:str) -> bool:
+		# returns true if message successfully sent, false if send failed.
+		try:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+				client.connect(self.dsuserver, port)
+
+				send = client.makefile('w')
+				recv = client.makefile('r')
+
+				send.write(message + '\r\n')
+				send.flush()
+
+				msg = recv.readline()
+
+
+	pass
+
+	def retrieve_new(self) -> list:
+	# returns a list of DirectMessage objects containing all new messages
+
+		pass
+
+	def retrieve_all(self) -> list:
+	# returns a list of DirectMessage objects containing all messages
+		pass
 	
+	def send_request(self, new_msg):
+		'''
+		Extra send function to connect retrieve_new and retrieve_all functions.
 		
-  def send(self, message:str, recipient:str) -> bool:
-    # returns true if message successfully sent, false if send failed.
-	try:
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-			client.connect(self.dsuserver, port)
-			
-			send = client.makefile('w')
-            		recv = client.makefile('r')
-			
-			send.write(profile + '\r\n')
-		    	send.flush()
-			
-		    	msg = recv.readline()
-			
-			
-    pass
+		new_msg will be the keywords associated to retrieve the desired information.
+		'''
+		try:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+				client.connect(self.dsuserver, port)
+
+				send = client.makefile('w')
+				recv = client.makefile('r')
+
+				send.write(new_msg + '\r\n')
+				send.flush()
+
+				msg = recv.readline()
 		
-  def retrieve_new(self) -> list:
-    # returns a list of DirectMessage objects containing all new messages
-    pass
- 
-  def retrieve_all(self) -> list:
-    # returns a list of DirectMessage objects containing all messages
-    pass
+
