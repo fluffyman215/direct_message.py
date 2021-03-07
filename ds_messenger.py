9f3dbdfcc863ac25dfd008cccf703923814fe9d4
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-03-07 13:08:48 -0800

    Update ds_messenger.py

--- ds_messenger.py
+++ ds_messenger.py
@@ -7,10 +7,24 @@ class DirectMessage:
 
 class DirectMessenger:
   def __init__(self, dsuserver=None, username=None, password=None):
-    self.token = None
+	self.token = None
+	self.dsuserver = None
+	self.username =
+	
 		
   def send(self, message:str, recipient:str) -> bool:
     # returns true if message successfully sent, false if send failed.
+	try:
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+			client.connect(server, port)
+			
+			send = client.makefile('w')
+            		recv = client.makefile('r')
+			
+			send.write(profile + '\r\n')
+		    	send.flush()
+			
+		    	msg = recv.readline()
     pass
 		
   def retrieve_new(self) -> list:
