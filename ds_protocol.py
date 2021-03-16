a967951bf8e6a8622c6d5a7d268520e410f80f63
fluffyman215 <53206825+fluffyman215@users.noreply.github.com>
2021-03-15 20:03:32 -0700

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -6,10 +6,13 @@ DataTuple = namedtuple('DataTuple', ['type','message'])
 def send_directmessage(user_token, msg, recipient):
 
     json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": str(time.time())}})
+    
     return json_directmessage
 
 def request_messages(user_token, msg):
+    
     json_request = json.dumps({"token": user_token, "directmessage": msg})
+    
     return json_request
 
 def extract_json_new(json_msg:str) -> DataTuple:
@@ -23,6 +26,7 @@ def extract_json_new(json_msg:str) -> DataTuple:
         message = json_obj['response']['message']
 
     except json.JSONDecodeError:
+        
         print("Json cannot be decoded.")
 
         return DataTuple(server_response, message)
