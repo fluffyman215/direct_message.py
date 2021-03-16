b2a9719015d6cefa20ca00d77f3dfd193f20a42a
podrago98 <79667946+podrago98@users.noreply.github.com>
2021-03-15 21:58:04 -0700

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -3,7 +3,7 @@ import json, time
 from collections import namedtuple
 DataTuple = namedtuple('DataTuple', ['type','message'])
 
-def send_direct_message(user_token, msg, recipient):
+def send_directmessage(user_token, msg, recipient):
 
     json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": str(time.time())}})
     
@@ -15,54 +15,53 @@ def request_messages(user_token, msg):
     
     return json_request
 
-def extract_response_typ(json_str:str) -> str:
-    typ = ''
-    token = ''
-    try:
-        json_obj = json.loads(json_str)
-        
-        typ = json_obj['response']['type']
-        message = json_obj['response']['message']
-
-        if typ == "ok" and message != "Direct message sent":
-            token = json_obj['response']['token']
-
-            return typ, token
-        else:
-            return typ
-
-    except json.JSONDecodeError:
-        print('JSON cannot be decoded')
-        return typ
-
-def extract_json_new(json_msg:str) -> DataTuple:
+def extract_json(json_msg:str) -> DataTuple:
     '''
     Call the json.loads function on a json string and convert it to a DataTuple object
     '''
-
-    try:
-        json_obj = json.loads(json_msg)
-        server_response = json_obj['response']['type']
-        message = json_obj['response']['message']
-
-    except json.JSONDecodeError:
-        
-        print("Json cannot be decoded.")
-
-        return DataTuple(server_response, message)
-
-def extract_json_all(json_msg:str) -> DataTuple:
-    '''
-    Call the json.loads function on a json string and convert it to a DataTuple object
-    '''
-
     try:
         json_obj = json.loads(json_msg)
         server_response = json_obj['response']['type']
         message = json_obj['response']['messages']
+        return message
 
     except json.JSONDecodeError:
+        
         print("Json cannot be decoded.")
 
         return DataTuple(server_response, message)
 
+
+def extract_response_typ(json_str:str) -> str:
+    typ = ''
+    try:
+        json_obj = json.loads(json_str)
+        typ = json_obj['response']['type']
+        return typ
+    except json.JSONDecodeError:
+        print('JSON cannot be decoded')
+        return typ
+
+def joinmsg(username:str, password:str) -> str:
+    """
+    This function will take in the parameters and create the join message.
+    """
+    msg = '{"join": {"username":"'+username+'", "password":"'+password+'", "token":"user_token"}}'
+    return msg
+
+def extract_token(json_str:str) -> str:
+    """
+    This function will take in the server's response, and return the server's public key for the ds_client to proceed.
+    """
+    typ = ''
+    msg = ''
+    token = None
+    try:
+        json_obj = json.loads(json_str)
+        typ = json_obj['response']['type']
+        msg = json_obj['response']['message']
+        if 'token' in json_obj['response']:
+            token = json_obj['response']['token']
+    except json.JSONDecodeError:
+        print('JSON cannot be decoded')
+    return token
