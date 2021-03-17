404516d7847d408a6236b2092c422a44af1ee355
dombomb-com <74215374+dombomb-com@users.noreply.github.com>
2021-03-17 15:28:24 -0700

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -3,22 +3,28 @@ import json, time
 from collections import namedtuple
 DataTuple = namedtuple('DataTuple', ['type','message'])
 
+"""
+Converts the send message into a json string.
+"""
 def send_directmessage(user_token, msg, recipient):
 
     json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": str(time.time())}})
     
     return json_directmessage
 
+"""
+Converts the request message into a json string.
+"""
 def request_messages(user_token, msg):
     
     json_request = json.dumps({"token": user_token, "directmessage": msg})
     
     return json_request
 
+"""
+Call the json.loads function on a json string and convert it to a DataTuple object
+"""
 def extract_json(json_msg:str) -> DataTuple:
-    '''
-    Call the json.loads function on a json string and convert it to a DataTuple object
-    '''
     try:
         json_obj = json.loads(json_msg)
         server_response = json_obj['response']['type']
@@ -31,7 +37,9 @@ def extract_json(json_msg:str) -> DataTuple:
 
         return DataTuple(server_response, message)
 
-
+"""
+Call the json.loads function on a json string and convert it to a DataTuple object and returns response type.
+"""
 def extract_response_typ(json_str:str) -> str:
     typ = ''
     try:
@@ -42,17 +50,17 @@ def extract_response_typ(json_str:str) -> str:
         print('JSON cannot be decoded')
         return typ
 
+"""
+This function will take in the parameters and create the join message.
+"""
 def joinmsg(username:str, password:str) -> str:
-    """
-    This function will take in the parameters and create the join message.
-    """
     msg = '{"join": {"username":"'+username+'", "password":"'+password+'", "token":"user_token"}}'
     return msg
 
+"""
+This function will take in the server's response, and return the server's public key for the ds_client to proceed.
+"""
 def extract_token(json_str:str) -> str:
-    """
-    This function will take in the server's response, and return the server's public key for the ds_client to proceed.
-    """
     typ = ''
     msg = ''
     token = None
