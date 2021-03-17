afafdeab77c6548c7378e29774c96c1eeb45efe1
dombomb-com <74215374+dombomb-com@users.noreply.github.com>
2021-03-17 16:20:43 -0700

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -3,28 +3,24 @@ import json, time
 from collections import namedtuple
 DataTuple = namedtuple('DataTuple', ['type','message'])
 
-"""
-Converts the send message into a json string.
-"""
+
 def send_directmessage(user_token, msg, recipient):
+    """Converts the send message into a json string."""
 
     json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": str(time.time())}})
     
     return json_directmessage
 
-"""
-Converts the request message into a json string.
-"""
 def request_messages(user_token, msg):
+    """Converts the request message into a json string."""
     
     json_request = json.dumps({"token": user_token, "directmessage": msg})
     
     return json_request
 
-"""
-Call the json.loads function on a json string and convert it to a DataTuple object and returns json message.
-"""
 def extract_json(json_msg:str) -> DataTuple:
+    """Call the json.loads function on a json string and convert it to a DataTuple object and returns json message."""
+    
     try:
         json_obj = json.loads(json_msg)
         server_response = json_obj['response']['type']
@@ -37,10 +33,9 @@ def extract_json(json_msg:str) -> DataTuple:
 
         return DataTuple(server_response, message)
 
-"""
-Call the json.loads function on a json string and convert it to a DataTuple object and returns response type.
-"""
 def extract_response_typ(json_str:str) -> str:
+    """Call the json.loads function on a json string and convert it to a DataTuple object and returns response type."""
+
     typ = ''
     try:
         json_obj = json.loads(json_str)
@@ -50,17 +45,15 @@ def extract_response_typ(json_str:str) -> str:
         print('JSON cannot be decoded')
         return typ
 
-"""
-This function will take in the parameters and create the join message.
-"""
 def joinmsg(username:str, password:str) -> str:
+    """This function will take in the parameters and create the join message."""
+    
     msg = '{"join": {"username":"'+username+'", "password":"'+password+'", "token":"user_token"}}'
     return msg
 
-"""
-This function will take in the server's response, and return the server's public key for the ds_client to proceed.
-"""
 def extract_token(json_str:str) -> str:
+    """This function will take in the server's response, and return the server's public key for the ds_client to proceed."""
+    
     typ = ''
     msg = ''
     token = None
