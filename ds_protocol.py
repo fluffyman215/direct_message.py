62df7229fc8c6a547d8275680cf02c961ca4a7ae
podrago98 <79667946+podrago98@users.noreply.github.com>
2021-03-17 17:29:54 -0700

    Update ds_protocol.py

--- ds_protocol.py
+++ ds_protocol.py
@@ -1,41 +1,47 @@
-#For JSON SCRIPT
 import json, time
 from collections import namedtuple
 DataTuple = namedtuple('DataTuple', ['type','message'])
 
 
 def send_directmessage(user_token, msg, recipient, time):
-    """Converts the send message into a json string."""
-
+    """
+    Takes in the message, recipient, and time and inserts it into the format the server is requesting.
+    That message is the send message.
+    Converts the send message into a json string.
+    """
     json_directmessage = json.dumps({"token":user_token, "directmessage": {"entry": msg,"recipient": recipient, "timestamp": time}})
-    
     return json_directmessage
 
+
 def request_messages(user_token, msg):
-    """Converts the request message into a json string."""
-    
+    """
+    Takes in the user_token and msg and inserts it into the format the server is requesting.
+    That message is the request message.
+    Converts the request message into a json string.
+    """
     json_request = json.dumps({"token": user_token, "directmessage": msg})
-    
     return json_request
 
+
 def extract_json(json_msg:str) -> DataTuple:
-    """Call the json.loads function on a json string and convert it to a DataTuple object and returns json message."""
-    
+    """
+    Call the json.loads function on a json string and convert it to a DataTuple object and returns json message.
+    """
     try:
         json_obj = json.loads(json_msg)
         server_response = json_obj['response']['type']
         message = json_obj['response']['messages']
         return message
-
     except json.JSONDecodeError:
-        
         print("Json cannot be decoded.")
-
         return DataTuple(server_response, message)
 
-def extract_response_typ(json_str:str) -> str:
-    """Call the json.loads function on a json string and convert it to a DataTuple object and returns response type."""
 
+def extract_response_typ(json_str:str) -> str:
+    """
+    Call the json.loads function on a json string and convert it to a DataTuple object and returns response type.
+    This function will take the response type from the server's message, either 'ok' or 'error'.
+    """
     typ = ''
     try:
         json_obj = json.loads(json_str)
@@ -45,15 +51,23 @@ def extract_response_typ(json_str:str) -> str:
         print('JSON cannot be decoded')
         return typ
 
+
 def joinmsg(username:str, password:str) -> str:
-    """This function will take in the parameters and create the join message."""
-    
+    """
+    Takes in the username and password and inserts it into the format the server is requesting.
+    That message is the join message.
+    Converts the join message into a json string.
+    """
     msg = '{"join": {"username":"'+username+'", "password":"'+password+'", "token":"user_token"}}'
     return msg
 
+
 def extract_token(json_str:str) -> str:
-    """This function will take in the server's response, and return the server's public key for the ds_client to proceed."""
-    
+    """
+    Call the json.loads function on a json string and convert it to a DataTuple object and returns response token.
+    This function will take the response token, the server provided.
+    This token is used to send messages to the server, to direct message, or to retrieve information.
+    """
     typ = ''
     msg = ''
     token = None
